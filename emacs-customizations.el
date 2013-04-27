277d137692bc083ca89f74d1c66a32f3ad6c74ca
Bryan Ash <bryan.a.ash@gmail.com>
2013-04-27 16:49:22 -0400

    configuraiton with coffee-mode submodule

---
+++ emacs-customizations.el
@@ -0,0 +1,23 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(auto-save-default nil)
+ '(coffee-tab-width 2)
+ '(cua-enable-cua-keys t)
+ '(desktop-save-mode t)
+ '(global-auto-revert-mode t)
+ '(ido-enable-flex-matching t)
+ '(ido-mode (quote both) nil (ido))
+ '(initial-frame-alist (quote ((top . 90) (left . 400) (width . 127) (height . 40))))
+ '(make-backup-files nil)
+ '(ns-command-modifier (quote meta))
+ '(scroll-bar-mode nil)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:background "black" :foreground "LightGoldenrod" :weight normal :height 140 :width normal :family "apple-monaco")))))
---
+++ init.el
@@ -0,0 +1,14 @@
+(setq my-emacs-init-file (or load-file-name buffer-file-name))
+(setq my-emacs-config-dir
+      (file-name-directory my-emacs-init-file))
+(setq my-emacs-init-dir
+      (expand-file-name "init.d" my-emacs-config-dir))
+
+(load (expand-file-name "coffee-mode.el" my-emacs-init-dir))
+
+(setq whitespace-action '(auto-cleanup)) ;; automatically clean up bad whitespace
+(setq whitespace-style '(trailing space-before-tab indentation empty space-after-tab)) ;; only show bad whitespace
+ 
+;; Set up 'custom' system
+(setq custom-file (expand-file-name "emacs-customizations.el" my-emacs-config-dir))
+(load custom-file)
