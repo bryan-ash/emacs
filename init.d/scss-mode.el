79dc9c9e2508e4fc86c66e6198eb20968e82b706
Bryan Ash <bryan.a.ash@gmail.com>
2015-09-22 15:49:35 -0400

    use scss-mode for .scss.erb files

--- init.d/scss-mode.el
+++ init.d/scss-mode.el
@@ -1,3 +1,4 @@
 (add-to-list 'load-path (expand-file-name "scss-mode" my-emacs-vendor-dir))
 (autoload 'scss-mode "scss-mode")
 (add-to-list 'auto-mode-alist '("\\.scss\\'" . scss-mode))
+(add-to-list 'auto-mode-alist '("\\.scss.erb\\'" . scss-mode))
